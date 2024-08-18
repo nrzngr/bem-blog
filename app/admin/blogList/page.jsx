88b3d55438e41a7698ccba784b83edab7d6082cf
nrzngr <109433343+nrzngr@users.noreply.github.com>
2024-08-18 19:46:36 +0700

    Update page.jsx

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
-const Page = () => {
+const page = () => {
 	const [blogs, setBlogs] = useState([]);
 
 	const fetchBlogs = async () => {
@@ -68,4 +68,4 @@ const Page = () => {
 	);
 };
 
-export default Page;
+export default page;
