5416f6f41ce50fb749b508e12bb396ff84830a1c
nrzngr <109433343+nrzngr@users.noreply.github.com>
2024-08-19 04:12:49 +0700

    Update Header.jsx

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
   return (
     <div className='py-5 px-5 md:px-12 lg:px-28'>
       <div className='flex justify-between items-center'>
-        <Image src={assets.logo} width={120} alt='' className='w-[130px] sm:w-auto'/>
+        <Image src={assets.logo} width={60} alt='' className='w-[130px] sm:w-auto'/>
       </div>
       <div className='text-center my-8'>
         <h1 className='text-3xl sm:text-5xl font-medium'>Berita Acara Terbaru</h1>
