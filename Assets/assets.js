99a171c772962534d6ac742d9d978f94f38f5260
nrzngr <109433343+nrzngr@users.noreply.github.com>
2024-08-19 04:20:53 +0700

    Update assets.js

--- Assets/assets.js
+++ Assets/assets.js
@@ -5,7 +5,6 @@ import instagram from "./instagram.png";
 import profile_icon from "./BEM-removebg.png";
 import logo from "./BEM-removebg.png";
 import arrow from "./arrow.png";
-import logo_light from "./logo_light.png";
 import blog_icon from "./blog_icon.png";
 import add_icon from "./add_icon.png";
 import email_icon from "./email_icon.png";
@@ -18,7 +17,6 @@ export const assets = {
 	profile_icon,
 	logo,
 	arrow,
-	logo_light,
 	blog_icon,
 	add_icon,
 	email_icon,
