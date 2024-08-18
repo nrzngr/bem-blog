e4b0c66308ede6ddd1836384b23f026e96cdf894
nrzngr <109433343+nrzngr@users.noreply.github.com>
2024-08-18 20:08:36 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Enable asset uploading to Vercel
-  asset: true,
   // Enable CSS modules
   css: {
     modules: true,
   },
-}
+  // Enable asset uploading to Vercel
+  asset: true,
+  // Enable production optimization
+  productionBrowserSourceMaps: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
