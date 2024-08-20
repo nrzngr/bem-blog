d9e3c658b87dafc95e3a1667c46079173e2f216d
nrzngr <109433343+nrzngr@users.noreply.github.com>
2024-08-20 23:53:14 +0700

    fix2

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center'>
       <Image src={assets.logo} alt='' width={120} />
       <p className='text-sm text-white'>All right reserved. Copyright @nrzngr.['Dept Kominfo BEM UBSI Karawang']</p>
-        <div className='flex'>
+        <div className='flex flex-row gap-2'>
             <Link href="https://www.tiktok.com/@bemubsi_kab.karawang?lang=en"><Image src={assets.tiktok_icon} alt='' width={40} /></Link>
             <Link href="https://www.instagram.com/bem_ubsikarawang/"><Image src={assets.instagram} alt='' width={40} /></Link>
         </div>
